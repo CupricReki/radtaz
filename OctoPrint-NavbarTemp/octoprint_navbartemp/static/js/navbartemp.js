263f047a5adaeb51b76aede93c578485af2b994d
Skyler Ogden <cupricreki@gmail.com>
2015-12-18 10:51:36 -0700

    Did shit

---
+++ OctoPrint-NavbarTemp/octoprint_navbartemp/static/js/navbartemp.js
@@ -0,0 +1,45 @@
+$(function() {
+    function NavbarTempViewModel(parameters) {
+        var self = this;
+
+        self.navBarTempModel = parameters[0];
+        self.global_settings = parameters[1];
+        self.raspiTemp = ko.observable();
+        self.isRaspi = ko.observable(false);
+
+        self.onBeforeBinding = function () {
+            self.settings = self.global_settings.settings.plugins.navbartemp;
+        };
+
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "navbartemp") {
+                return;
+            }
+
+            if (!data.hasOwnProperty("israspi")) {
+                self.isRaspi(false);
+                return;
+            } else {
+                self.isRaspi(true);
+            }
+
+            self.raspiTemp(_.sprintf("Raspi: %.1f&deg;C", data.raspitemp));
+        };
+    }
+
+    ADDITIONAL_VIEWMODELS.push([
+        NavbarTempViewModel, 
+        ["temperatureViewModel", "settingsViewModel"],
+        ["#navbar_plugin_navbartemp", "#settings_plugin_navbartemp"]
+    ]);
+});
+
+function formatBarTemperature(toolName, actual, target) {
+    var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
+
+    if (target) {
+        output += " \u21D7 " + _.sprintf("%.1f&deg;C", target);
+    }
+
+    return output;
+};
